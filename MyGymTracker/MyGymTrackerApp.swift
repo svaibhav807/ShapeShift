54c7dc8c33d9d5066d1cde58fbad114070a66baa
svaibhav807 <svaibhav807@gmail.com>
2023-01-09 15:52:58 +0530

    Initial Commit

---
+++ MyGymTracker/MyGymTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyGymTrackerApp.swift
+//  MyGymTracker
+//
+//  Created by Vaibhav Singh on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct MyGymTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
